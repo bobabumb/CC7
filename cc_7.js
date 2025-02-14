67f16bb3b612b86bc079c0bc7e3c74ad07bb9c27
kenn <kennnguyen@usf.edu>
2025-02-14 15:13:01 -0500

    task 2 - employee hourly wage calculations

--- cc_7.js
+++ cc_7.js
@@ -2,5 +2,14 @@
 const calculateInvoice = (subtotal, taxRate, discount) =>
     `total invoice: $${((subtotal * (1 + taxRate)) - discount).toFixed(2)}`;
 // test data
-console.log(calculateInvoice(100, 0.08, 5));
-console.log(calculateInvoice(500, 0.1, 20));
+console.log(calculateInvoice(100, 0.08, 5)); // expected output: "total invoice: $103.00"
+console.log(calculateInvoice(500, 0.1, 20)); // expected output: "total invoice: $530.00"
+
+// task 2 function expression
+const calculateHourlyWage = function(salary, hoursPerWeek) {
+    const hourlyWage = salary / (hoursPerWeek * 52);
+    return `hourly wage: $${hourlyWage.toFixed(2)}`;
+};
+// test data
+console.log(calculateHourlyWage(52000, 40)); // expected output: "hourly wage: $25.00"
+console.log(calculateHourlyWage(75000, 35)); // expected output: "hourly wage: $41.21"
